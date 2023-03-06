181806752ebc63d9399efadc75a72a3e2dea4c26
Mikhailo Sopriko <identityapproved@gmail.com>
2023-03-06 20:58:07 +0200

    LRU(base setup)

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,45 @@
+type Node<T> = {
+  value: T,
+  prev?: Node<T>,
+  next?: Node<T>,
+};
+
+function createNode<V>(value: V): Node<V> {
+  return {value};
+}
+
 export default class LRU<K, V> {
-    private length: number;
+  private length: number;
+  private head?: Node<V>;
+  private tail?: Node<V>;
 
+  private lookup: Map<K, Node<V>>;
+  private reverseLookup: Map<Node<V>, K>;
     
+  constructor() {
+    this.length = 0;
+    this.head = this.tail = undefined;
+    this.lookup = new Map<K, Node<V>>();
+    this.reverseLookup = new Map<Node<V>, K>();
+  }
 
-    constructor() {
-    }
+  update(key: K, value: V): void {
 
-    update(key: K, value: V): void {
+    // does it exist?
+    //
+    // if it doesn't => incert
+    //    - check capacity and evict if over
+    // if it does => update to the front of the list and update the value
+    //
+  }
 
-}
-    get(key: K): V | undefined {
+  get(key: K): V | undefined {
 
-}
+    // check the cache for existance
+
+    // update the value we found and move it to the front 
+
+    // return out the value found or undefined if not exist
+  }
+
 }
